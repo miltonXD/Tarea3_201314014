f6634b0142db72271a1fb1de132fab5814679267
miltonXD <javierxd75@hotmail.com>
2014-08-19 22:52:56 +0200

    Proyecto en Eclipse
    Programa que verifica si un numero es par o impar

---
+++ Tarea3/src/Tarea3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//Miton Javier Navarro
+public class Tarea3 {
+	public static void main(String [ ] args)
+	{
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingrese un numero");
+		n=sc.nextInt();
+		    if(n % 2 == 0){
+			     System.out.println("El numero es par");
+		      }
+		      else{
+			     System.out.println("El numero es impar");
+		      }
+		
+	}
+
+}
